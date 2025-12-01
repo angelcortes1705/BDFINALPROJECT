5d9c26d8971e2ecb57283115349fc786938c574b
Ángel Alí Cortés González <ali.cortes@iteso.mx>
2025-12-01 17:45:50 -0600

    Reinicio

--- services/mongo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-module.exports = {
-  connect: async (uri) => {
-    await mongoose.connect(uri, { useNewUrlParser:true, useUnifiedTopology:true });
-    console.log('Mongo connected');
-  },
-  mongoose
-};
